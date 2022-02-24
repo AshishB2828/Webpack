1c1732c48f8f5cfc72b10a8a259f433a235e8999
Ashish <unnib2828@gmail.com>
2022-02-24 18:18:16 +0530

    style && css -loader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { AlertService } from "./app/alert.services";
 import { run } from "./app/app";
 import { ComponentService } from "./app/component.services";
+import "./main.css";
 
 const alertService = new AlertService();
 const componentService = new ComponentService();
