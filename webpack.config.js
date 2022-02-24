ad59fe738c8216a594eac48693eaf3e45611cd24
Ashish <unnib2828@gmail.com>
2022-02-24 18:57:06 +0530

    plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: "./src/index.js",
     output: {
-        filename:"main.js",
+        filename:"main.[contenthash].js",
         path: path.resolve(__dirname, "dist")
     },
     module: {
@@ -17,5 +18,8 @@ module.exports = {
         },
         ],
     },
+    plugins: [new HtmlWebpackPlugin({
+        template: "./src/template.html"
+    }), ],
     
 }
